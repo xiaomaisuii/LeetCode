c110332701aa14f71846fdf42f31564e8fde0ef1
liwenjie001 <1554616416@qq.com>
2019-08-18 17:36:36 +0800

    1. 优化了算法 2. 具体是考虑把双层for干掉 3. 用一个变量记住第一个数是什么，然后for对比，如果一样就计数，不一样的情况就重新赋值第一个数，知道for循环结束，算法的时间复杂度一下就从O(n^2)到了O(n) 效率提升了不少。

--- src/String/CountAndSay.java
+++ src/String/CountAndSay.java
@@ -19,6 +19,7 @@ public class CountAndSay {
      * @return
      */
     public String countAndSay(int n) {
+        // 这个就是递归的出口
         if (n==1) return "1";
         // 规律就是上次出现的然后咱们数一下，就是这次出现的结果，并且是递归调用。
         String str = countAndSay(n-1);
@@ -39,10 +40,41 @@ public class CountAndSay {
         return tmpStr;
     }
 
+    /**
+     * 优化一下算法把其中的双重for干掉。
+     * 思想就是把第一个数拿出来，然后循环字符串一样的有多少个就记录下来，如果遇到不一样的,那么就带吧算出来之前有多少个一样的了
+     * 然后就把第一个数字重新赋值,依此下去就代替双层for的作用了.
+     * @param n
+     * @return
+     */
+    public String countAndSay1(int n) {
+        if (n==1) return "1";
+        // 规律就是上次出现的然后咱们数一下，就是这次出现的结果，并且是递归调用。
+        String str = countAndSay1(n-1);
+//        String tmpStr = "";
+        char c = str.charAt(0);
+        // 这是个对象，必须低new才能创建啊；
+        StringBuilder tmpStr = new StringBuilder();
+        int count = 1;
+        for (int i = 1; i < str.length(); i++) {
+            if (c== str.charAt(i)){
+                count++;
+            }else {
+                tmpStr.append(count).append(c);
+                count = 1;
+                c = str.charAt(i);
+            }
+        }
+        tmpStr.append(count).append(c);
+        return String.valueOf(tmpStr);
+    }
+
     public static void main(String[] args) {
         CountAndSay countAndSay = new CountAndSay();
         int n = 6;
         String s = countAndSay.countAndSay(n);
+        String s1 = countAndSay.countAndSay1(n);
+        System.out.println(s1);
         System.out.println(s);
     }
 }
