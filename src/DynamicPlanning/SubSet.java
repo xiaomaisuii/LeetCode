1da34134afcf654c0cf8a337818317d64419f58c
liwenjie001 <1554616416@qq.com>
2019-08-29 21:47:12 +0800

    1. 动态规划实现

--- src/DynamicPlanning/SubSet.java
+++ src/DynamicPlanning/SubSet.java
@@ -1,5 +1,7 @@
 package DynamicPlanning;
 
+import sun.font.TrueTypeFont;
+
 /**
  * @ Author: Mr.Li
  * @ Date: 2019-08-29 20:39
@@ -21,11 +23,42 @@ public class SubSet {
         }
     }
 
+    /**
+     * 用动态规划实现
+     * 动态规划就是解决递归计算的重复子序列。
+     * @param arr
+     * @param s
+     * @return
+     */
+    public boolean subSet1(int[] arr, int s) {
+        boolean[][] subset = new boolean[arr.length][s + 1];
+        for (int j = 0; j < arr.length; j++) {
+            subset[j][0] = true;
+        }
+        for (int j = 0; j < s+1; j++) {
+            subset[0][j] = false;
+        }
+        subset[0][arr[0]] = true;
+        for (int i = 1; i < arr.length ; i++) {
+            for (int j = 1; j < s+1; j++) {
+                if (arr[i] > j) {
+                    subset[i][j] = subset[i - 1][j];
+                }else {
+                    boolean a = subset[i - 1][j - arr[i]];
+                    boolean b = subset[i - 1][j];
+                    subset[i][j] = a || b;
+                }
+            }
+        }
+        return subset[arr.length-1][s];
+    }
+
     public static void main(String[] args) {
         SubSet subSet = new SubSet();
         int[] arr = {1, 3, 4, 5, 6, 7, 8, 1};
         boolean b = subSet.subSet(arr, arr.length-1, 10);
-        if (b) {
+        boolean b1 = subSet.subSet1(arr, 10);
+        if (b1) {
             System.out.println("可以");
         }else {
             System.out.println("不可以");
